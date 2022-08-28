e1c5e6c06f3be656b77d3e4d69e49146119c5120
laurafcamargos <laurafernandes1465@gmail.com>
2022-08-28 13:29:44 -0300

    algoritmo de ordenação selection e bubble  com sentinela
    primeira exercício da disciplina realizado em trio

---
+++ Merge.c
@@ -0,0 +1,86 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+void merge(int a[], int n);
+void mergerecursivo(int a[], int inicio, int fim, int b[]);
+
+void merge(int a[], int n) // qntd de elementos do vetor
+{
+    int *b = (int*)malloc(n * sizeof(int));
+    mergerecursivo(a, 0, n - 1, b); // indice do ultimo elemento, b eh o vetor auxiliar
+    free(b);
+}
+void mergerecursivo(int *a, int inicio, int fim, int b[])
+{
+    if (inicio >= fim) // caso base: aonde deixa de ser recursiva
+    {
+        return;
+    }
+    int m = (inicio + fim) / 2;
+    mergerecursivo(a, inicio, m, b);
+    mergerecursivo(a, m + 1, fim, b); // tenho dois vetores ordenados
+    int i1 = inicio;
+    int i2 = m + 1;
+    int j = 0; // aonde em b eu vou colocar o menor elemento dos outros dois vetores
+    while (i1 <= inicio && i2 <= fim)
+    {
+        if (a[i1] <= a[i2])
+        {
+            b[j] = a[i1];
+            i1++;
+        }
+        else
+        {
+            b[j = a[i2]];
+            i2++;
+        }
+        j++;
+    }
+    while (i1 <= m)
+    {
+        b[j] = a[i1];
+        i1++;
+        j++;
+    }
+    while (i2 <= fim)
+    {
+        b[j] = a[i1];
+        i1++;
+        j++;
+    }
+    j = 0;
+    for (int i = inicio; i <= fim; i++)
+    {
+        a[i] = b[j];
+        j++;
+    }
+}
+void printa_vetor(int *vetor, int n)
+{
+    int i;
+    for (i = 0; i < n; i++)
+    {
+        printf("%d ", vetor[i]);
+    }
+}
+int main() {
+  int n;
+  scanf("%d",&n);
+  int vetor[n];
+   srand(time(NULL));
+
+  for (int i = 0; i < n; i++)
+  {
+    vetor[i] = rand () % 100;
+  }
+  for (int i = 0; i < n; i++)
+  {
+    printf("%d ",vetor[i]);
+  }
+  printf("\n");
+  merge(vetor,n);
+  printa_vetor(vetor,n);
+  
+}
--- SelecBubble.c
+++ SelecBubble.c
@@ -5,7 +5,7 @@
 void bubble_sort(int *vetor, int n);
 void selection_sort(int *vetor, int max);
 int *ler_vetor(int *vetor, int n);
-void *printa_vetor(int *vetor, int n);
+void printa_vetor(int *vetor, int n);
 int *ler_vetor(int *vetor, int n)
 {
     for (int i = 0; i < n; i++)
@@ -17,20 +17,30 @@ int *ler_vetor(int *vetor, int n)
 
 void bubble_sort(int *vetor, int n)
 {
-    int i, j, aux;
+    int i, j, aux, troca;
+    int sentinela;
+    int p = n - 1;
 
-    for (i = n - 1; i > 0; i--)
+    for (i = n - 1; i >= 0; i--)
     {
-
-        for (j = 0; j < i; j++)
+        troca = 0;
+        for (j = 0; j < p; j++)
         {
-
+            printf("C %d %d\n", j, j + 1);
             if (vetor[j] > vetor[j + 1])
             {
                 aux = vetor[j];
                 vetor[j] = vetor[j + 1];
                 vetor[j + 1] = aux;
+                sentinela = j;
+                printf("T %d %d\n", j, j + 1);
+                troca++;
             }
+        }
+        p = sentinela;
+        if (troca == 0)
+        {
+            break;
         }
     }
 }
@@ -43,9 +53,9 @@ void selection_sort(int *vetor, int n)
         menor = i;
         for (j = i + 1; j < n; j++)
         {
+            printf("C %d %d\n", menor, j);
             if (vetor[j] < vetor[menor])
             {
-                printf("C %d %d\n", i,j);
                 menor = j;
             }
         }
@@ -54,10 +64,11 @@ void selection_sort(int *vetor, int n)
             aux = vetor[i];
             vetor[i] = vetor[menor];
             vetor[menor] = aux;
+            printf("T %d %d\n", i, menor);
         }
     }
 }
-void *printa_vetor(int *vetor, int n)
+void printa_vetor(int *vetor, int n)
 {
     int i;
     for (i = 0; i < n; i++)
