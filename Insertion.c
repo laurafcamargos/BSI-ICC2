53e580d34328932982b9c55a7c10c99f2c877f70
laurafcamargos <laurafernandes1465@gmail.com>
2022-08-28 13:32:28 -0300

    método de seleção Insertion(melhor entre os quadráticos)

--- Insertion.c
+++ Insertion.c
@@ -2,42 +2,45 @@
 #include <stdlib.h>
 #include <time.h>
 
-void ins(int a[], int n){
-for(int p = 1; p < n; p++){
-  int x = a[p];
-  int i = p-1;
-  while(i >= 0 && x < a[i]){
-    a[i+1] = a[i];
-    i--;
-  }
-  a[i+1] = x;
+void ins(int a[], int n)
+{
+  for (int p = 1; p < n; p++)
+  {
+    int x = a[p];
+    int i = p - 1;
+    while (i >= 0 && x < a[i])
+    {
+      a[i + 1] = a[i];
+      i--;
+    }
+    a[i + 1] = x;
   }
 }
-//adaptavel e estavel
+// adaptavel e estavel
 void printa_vetor(int *vetor, int n)
 {
-    int i;
-    for (i = 0; i < n; i++)
-    {
-        printf("%d ", vetor[i]);
-    }
+  int i;
+  for (i = 0; i < n; i++)
+  {
+    printf("%d ", vetor[i]);
+  }
 }
-int main() {
+int main()
+{
   int n;
-  scanf("%d",&n);
+  scanf("%d", &n);
   int vetor[n];
-   srand(time(NULL));
+  srand(time(NULL));
 
   for (int i = 0; i < n; i++)
   {
-    vetor[i] = rand () % 100;
+    vetor[i] = rand() % 100;
   }
   for (int i = 0; i < n; i++)
   {
-    printf("%d ",vetor[i]);
+    printf("%d ", vetor[i]);
   }
   printf("\n");
-  ins(vetor,n);
-  printa_vetor(vetor,n);
-  
+  ins(vetor, n);
+  printa_vetor(vetor, n);
 }
