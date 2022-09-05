b33ccf7f557cdb7b2e55f352f25ed6d66ab56190
laurafcamargos <laurafernandes1465@gmail.com>
2022-09-05 16:19:05 -0300

    Método de ordenação Merge Sort correto

--- Merge.c
+++ Merge.c
@@ -3,55 +3,78 @@
 #include <time.h>
 
 void merge(int a[], int n);
-void mergerecursivo(int a[], int inicio, int fim, int b[]);
+void merge2(int a[], int c, int f, int b[]);
 
 void merge(int a[], int n) // qntd de elementos do vetor
 {
     int *b = (int *)malloc(n * sizeof(int));
-    mergerecursivo(a, 0, n - 1, b); // indice do ultimo elemento, b eh o vetor auxiliar
+    merge2(a, 0, n - 1, b); // indice do ultimo elemento, b eh o vetor auxiliar
     free(b);
 }
-void mergerecursivo(int *a, int inicio, int fim, int b[])
-{
-    if (inicio >= fim) // caso base: aonde deixa de ser recursiva
-    {
-        return;
+/*
+    Função Recursiva do Algoritmo Merge Sort
+
+    Parâmetros:
+        - a: vetor a ser ordenado;
+        - c: começo do vetor (incluso);
+        - f: fim do vetor (incluso);
+        - b: vetor extra pré alocado para cópias;
+*/
+void merge2(int a[], int c, int f, int b[]) {
+
+    // Caso base: 
+    if (c >= f) {
+        // ou o vetor é de tamanho 0 ou de
+        // tamanho 1, ou seja, já está ordenado
+        return;   
     }
-    int m = (inicio + fim) / 2;
-    mergerecursivo(a, inicio, m, b);
-    mergerecursivo(a, m + 1, fim, b); // tenho dois vetores ordenados
-    int i1 = inicio;
-    int i2 = m + 1;
-    int j = 0; // aonde em b eu vou colocar o menor elemento dos outros dois vetores
-    while (i1 <= inicio && i2 <= fim)
-    {
-        if (a[i1] <= a[i2])
-        {
+
+    // Cálculo da posição central do vetor:
+    int m = (c + f) / 2;
+
+    // Chamadas recursivas para as 2 metades:
+    merge2(a, c, m, b);
+    merge2(a, m + 1, f, b);
+
+    // Inicialização dos apontadores para os vetores 
+    // da esquerda e da direita, sabendo que o primeiro vetor
+    // vai de c à m e que o segundo vetor de m + 1 à f:
+    int i1 = c;
+    int i2 = m + 1; 
+    int j = 0;
+
+    // Junção ordenada e estável dos 2 vetores:
+    while (i1 <= m && i2 <= f) {
+        if (a[i1] <= a[i2]) {
             b[j] = a[i1];
             i1++;
         }
-        else
-        {
+        else {
             b[j] = a[i2];
             i2++;
         }
         j++;
     }
-    while (i1 <= m)
-    {
+
+    // Junção dos elementos restantes do primeiro
+    // vetor (se ele não tiver terminado):
+    while (i1 <= m) {
         b[j] = a[i1];
         i1++;
         j++;
     }
-    while (i2 <= fim)
-    {
-        b[j] = a[i1];
-        i1++;
+
+    // Junção dos elementos restantes do segundo
+    // vetor (se ele não tiver terminado):
+    while (i2 <= f) {
+        b[j] = a[i2];
+        i2++;
         j++;
     }
+
+    // Cópia dos elementos ordenados de b para a:
     j = 0;
-    for (int i = inicio; i <= fim; i++)
-    {
+    for(int i = c; i <= f; i++) {
         a[i] = b[j];
         j++;
     }
